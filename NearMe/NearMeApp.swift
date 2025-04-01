db758c9c2a23ff0404a4d6453486a71447148222
superkookai <weerawut@live.com>
2025-04-01 12:14:14 +0700

    Initial Commit

---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by Weerawut Chaiyasomboon on 01/04/2568.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
